dc40b9dac979a6a24a4e355fab6a6a10674495c6
Mikołaj Sodzawiczny <mikolaj@worldanvil.com>
2024-10-11 16:34:55 +0200

    i think its all i got

--- gtk/main.c
+++ gtk/main.c
@@ -1,10 +1,34 @@
 #include <gtk/gtk.h>
 
-static void app_activate(GApplication *app, gpointer *user_data) {
-  GtkWidget *win;
+static void click2_cb(GtkButton *btn, GtkWindow *win) {
+  gtk_window_destroy(win);
+}
 
-  win = gtk_window_new();
-  gtk_window_set_application(GTK_WINDOW(win), GTK_APPLICATION(app));
+static void app_activate(GApplication *app) {
+  GtkWidget *win;
+  GtkWidget *scr;
+  GtkWidget *box;
+  GtkWidget *btn2;
+  GtkWidget *tv;
+  GtkTextBuffer *tb;
+  gchar *text;
+
+  text = "Once upon a time, there was an old man who was called "
+         "Taketori-no-Okina. ";
+  win = gtk_application_window_new(GTK_APPLICATION(app));
+  gtk_window_set_title(GTK_WINDOW(win), "Simple Text Editor");
+  gtk_window_set_default_size(GTK_WINDOW(win), 400, 300);
+
+  scr = gtk_scrolled_window_new();
+  gtk_window_set_child(GTK_WINDOW(win), scr);
+
+  tv = gtk_text_view_new();
+  tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
+  gtk_text_buffer_set_text(tb, text, -1);
+  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
+
+  gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scr), tv);
+
   gtk_window_present(GTK_WINDOW(win));
 }
 
